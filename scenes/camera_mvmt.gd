c5c9efa9296f63f55851628dc5150ae550585c13
Santum <39064333+santumerino@users.noreply.github.com>
2025-07-31 19:46:39 -0300

    add: Camera movement

---
+++ scenes/camera_mvmt.gd
@@ -0,0 +1,10 @@
+extends Camera3D
+
+@onready var camera = $"."
+const MOUSE_SENS = 0.00004 # Magic numbers!
+
+func _input(event):
+	if event is InputEventMouseMotion:
+			camera.rotate_y(-event.relative.x * MOUSE_SENS)
+			camera.rotate_x(-event.relative.y * MOUSE_SENS)
+			camera.rotation.z = 0
