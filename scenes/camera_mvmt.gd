88e03948ed0971cd18a196bb8f80a40a6fe14e00
Santum <39064333+santumerino@users.noreply.github.com>
2025-07-31 20:43:35 -0300

    Add: player """model"""
    With animations!

--- scenes/camera_mvmt.gd
+++ scenes/camera_mvmt.gd
@@ -7,4 +7,4 @@ func _input(event):
 	if event is InputEventMouseMotion:
 			camera.rotate_y(-event.relative.x * MOUSE_SENS)
 			camera.rotate_x(-event.relative.y * MOUSE_SENS)
-			camera.rotation.z = 0
+			camera.rotation.z = 0 # NOTE: When the player switches sides, instead of rotating the camera, rotate the decorations. EZ!
