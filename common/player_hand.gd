601ceaa9ae0fa0a10a5827871bb8c9eed6bef1c4
Fabian Friedrich <friedrich.fabian@gmx.net>
2025-07-31 18:56:38 +0200

    Add: display of players hand and card hover effect

--- common/player_hand.gd
+++ common/player_hand.gd
@@ -1,9 +1,17 @@
 extends Node
 
 @export var hand: Array[Card]
+@onready var hand_parent: Node3D = $Cards
+var card_prefab = preload("res://cards/card.tscn")
 @onready var deck: Deck = $Deck
 
 func _ready():
-	print(deck)
 	deck.shuffle_cards() 
 	hand.append_array(deck.draw_cards(6))
+
+	for i in hand.size(): 
+		var card_res = hand[i]
+		var card = card_prefab.instantiate()
+		hand_parent.add_child(card)
+		card.position.x = i * 0.8 
+		card.emit_signal("update", card_res) 
