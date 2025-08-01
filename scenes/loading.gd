61d4d9d74c5fca6a5404314e23c811512fbb490f
Santum <39064333+santumerino@users.noreply.github.com>
2025-07-31 22:23:58 -0300

    add: (unfinished) Title screen and loading scene

---
+++ scenes/loading.gd
@@ -0,0 +1,24 @@
+extends Control
+
+const TARGET_SCENE = "res://scenes/game.tscn"
+
+var loading_status : int
+var progress : Array[float]
+
+@onready var progress_bar : ProgressBar = $ProgressBar
+
+func _ready() -> void:
+	ResourceLoader.load_threaded_request(TARGET_SCENE) # "Hello, may I have one game scene please?"
+
+func _process(delta: float) -> void:
+	loading_status = ResourceLoader.load_threaded_get_status(TARGET_SCENE, progress)
+	
+	match loading_status:
+		ResourceLoader.THREAD_LOAD_IN_PROGRESS:
+			progress_bar.value = progress[0] * 100 # Update the value
+		ResourceLoader.THREAD_LOAD_LOADED:
+			# When done loading, change to the game
+			get_tree().change_scene_to_packed(ResourceLoader.load_threaded_get(TARGET_SCENE))
+		ResourceLoader.THREAD_LOAD_FAILED:
+			# If something's gone wrong, pretend nothing happened and go back to the title screen lol
+			get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
