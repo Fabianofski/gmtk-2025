1507ea93b8233b1c5b74a5848936abe3d5a54e68
Fabian Friedrich <friedrich.fabian@gmx.net>
2025-08-02 12:42:07 +0200

    Add: better round info display

--- common/game_manager.gd
+++ common/game_manager.gd
@@ -4,11 +4,17 @@ class_name Game
 var selected_cards: Array[Card] = []
 var last_played: Array[Card] = []
 @export var health: int = 10
-@onready var round_info: Label = $RoundInfo
 @onready var state_info: Label = $"State/StateInfo"
 @onready var goal_info: Label = $"Goal/GoalInfo"
 @onready var health_label: Label = $"Health/Health Label"
 @onready var score_label: Label = $"Score/Score Label"
+
+@onready var round_hearts: Control = $"RoundInfo/Hearts"
+@onready var round_hearts_label: Label = $"RoundInfo/Hearts/Hearts"
+@onready var round_atk: Control = $"RoundInfo/Atk"
+@onready var round_atk_label: Label = $"RoundInfo/Atk/Atk"
+@onready var round_dfs: Control = $"RoundInfo/Dfs"
+@onready var round_dfs_label: Label = $"RoundInfo/Dfs/Dfs"
 
 @export var score: int = 0
 @export var current_round: int = 0
@@ -23,9 +29,9 @@ func _init():
 	SignalBus.deselect_card.connect(deselect_card)	
 
 func _ready():
-	goal_info.text = "Score %d more points to win" % (max(0, target_score.call() - score))
+	update_label()
 
-func _process(delta: float) -> void: # Update score display
+func _process(_delta: float) -> void: # Update score display
 	if int(score_label.text) < score:
 		score_label.text = str(int(score_label.text)+1).pad_zeros(9)
 	elif int(score_label.text) > score:
@@ -41,14 +47,22 @@ func deselect_card(card: Card):
 	update_label()
 
 func update_label(): 
-	var preview = calc_atk_dfs_values(selected_cards)
-	print(preview)
+	var hand = calc_atk_dfs_values(selected_cards)
+	var old_hand = calc_atk_dfs_values(last_played)
 	if state == STATE.Attack: 
-		round_info.text = "Atk: " + str(preview['atk']) + ", Score: " + str(preview['score'])
+		round_dfs.visible = false
+		round_hearts.visible = false
+		round_atk_label.text = str(hand['atk'])
 		goal_info.text = "Score %d more points to win" % (max(0, target_score.call() - score))
 		state_info.text = "ATK"
 	else: 
-		round_info.text = "Def: " + str(preview['dfs'])
+		round_dfs.visible = true
+		round_hearts.visible = true 
+		round_dfs_label.text = str(hand['dfs'])
+		round_atk_label.text = str(old_hand['atk'])
+		var diff = hand['dfs'] - old_hand['atk']
+		round_hearts.visible = diff < 0
+		round_hearts_label.text = str(diff)
 		state_info.text = "DEF"
 	health_label.text = str(health).pad_zeros(4)
 
