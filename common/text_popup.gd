47b091639a52581cc4e878a79bfc083458a00861
Fabian Friedrich <friedrich.fabian@gmx.net>
2025-08-02 11:15:10 +0200

    Add: attack and defense sound

--- common/text_popup.gd
+++ common/text_popup.gd
@@ -1,12 +1,24 @@
 extends Control 
 
 @onready var label: Label = $"Label"
+@onready var attack_sound: AudioStreamPlayer3D = $"AttackSound"
+@onready var defense_sound: AudioStreamPlayer3D = $"DefenseSound"
 
 func _init():
 	SignalBus.game_won.connect(func(x): show_text("Round %d" % (x + 1)))
+	SignalBus.next_round_started.connect(round_started)
 
 func _ready():
 	show_text("Round 1")
+
+func round_started(state: Game.STATE): 
+	match(state): 
+		Game.STATE.Attack: 
+			show_text("Attack") 
+			attack_sound.play()
+		Game.STATE.Defense: 
+			show_text("Defense") 
+			defense_sound.play()
 
 func show_text(msg: String): 
 	label.text = msg 
