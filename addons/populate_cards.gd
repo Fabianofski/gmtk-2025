51d8f3dd68497e1d6ec4eb57ab6f7427f16f8a50
Fabian Friedrich <friedrich.fabian@gmx.net>
2025-08-03 13:40:44 +0200

    Add: cards can now be in multiple directories

--- addons/populate_cards.gd
+++ addons/populate_cards.gd
@@ -4,21 +4,27 @@ extends EditorScript
 func _run():
 	var node = get_scene().get_node("Player Hand/Deck") 
 	var cards_folder = "res://cards/res"
+	
+	var cards = look_for_cards(cards_folder)	
+	node.cards = cards
+	print("Done. Imported %d cards." % cards.size())
+
+func look_for_cards(cards_folder): 
 	var dir := DirAccess.open(cards_folder)
 	var cards = []
-	
 	if dir:
 		dir.list_dir_begin()
 		while true:
 			var file_name = dir.get_next()
+			var path = cards_folder + "/" + file_name
 			if file_name == "":
 				break
 			if dir.current_is_dir():
-				continue
+				cards.append_array(look_for_cards(path))
 			if file_name.ends_with(".tres") or file_name.ends_with(".res"):
-				var card = load(cards_folder + "/" + file_name)
+				var card = load(path)
 				if card is Resource:
 					cards.append(card)
 		dir.list_dir_end()
-	
-	node.cards = cards
+
+	return cards
