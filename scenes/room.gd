b26870bbf7bd69ee13003b42ab097bc8388adb5f
Fabian Friedrich <friedrich.fabian@gmx.net>
2025-08-03 15:38:49 +0200

    Add: tweening of decorations

--- scenes/room.gd
+++ scenes/room.gd
@@ -6,8 +6,9 @@ func _ready() -> void:
 	SignalBus.next_round_started.connect(_switch_sides)
 
 func _switch_sides(state):
+	var tween := create_tween()
 	match state:
 		Game.STATE.Attack:
-			decoration.rotation_degrees.y = 0
+			tween.tween_property(decoration, "rotation_degrees:y", 0, 0.5)
 		Game.STATE.Defense:
-			decoration.rotation_degrees.y = -180
+			tween.tween_property(decoration, "rotation_degrees:y", -180, 0.5)
