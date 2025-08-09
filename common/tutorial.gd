07946c00b124b2186d3a299a12a99b696eef19b8
Santum <39064333+santumerino@users.noreply.github.com>
2025-08-09 17:01:18 -0300

    add: New tutorial

--- common/tutorial.gd
+++ common/tutorial.gd
@@ -1,69 +1,101 @@
 extends Control
 
+@onready var tutorial_panel: NinePatchRect = $"Tutorial Panel"
 @onready var tutorial_text: Label = $"Tutorial Panel/Tutorial Text"
 
 @onready var back_button: Button = $"Tutorial Panel/Back Button"
 @onready var next_button: Button = $"Tutorial Panel/Next Button"
 
-var current_displayed_string : int = 1
+@onready var mouse_input_blocker: ColorRect = $"Mouse Input Blocker"
 
-# ALERT: Hardcoded strings!!!
-const STRING_ARRAY : Array = ["OK!",
+var line_to_display: int = 0
 
-"Are you familiar with asymmetrical card games involving time travel?",
+# Centre, bottom right, top left, top right, top
+var positions: Array = [Vector2(422, 277), Vector2(815, 450), Vector2(28, 100), Vector2(815, 100), Vector2(422, 112)]
 
-"In this game, the attacker will draw up to six cards, and then score points based on the number on the card.
-Then, the defender will draw up to six cards, and try to defend against their attack.
+func _ready() -> void:
+	SignalBus.next_round_started.connect(_should_be_shown)
+	_refresh_text()
 
-The goal is for the attacker to reach the high score, and the defender to not lose all their health.",
-
-"However, this is no ordinary card game. Because your opponent... is YOURSELF!
-
-A time machine malfunction has resulted in you being forced to play against yourself on a loop, as both the attacker AND the defender.",
-
-"Play as many attack cards as you want to rack up a combo, then travel TO THE FUTURE and defend yourself from your own attack!
-Then, go BACK TO THE PAST, and set up a new attack!
-
-Reach the score win condition before shedding all your cards, or else the timestream will dissipate FOREVER.",
-
-"And with that, that's the end of this tutorial!",
-
-"P.S.: If you're ever confused on what to do, look at the ticker at the top of the screen!",
-
-""]
-
-func _ready():
-	if SignalBus.tutorial_shown: 
-		self.visible = false
-	else: 
-		SignalBus.tutorial_shown = true
-
-func _process(_delta: float) -> void:
-	tutorial_text.text = STRING_ARRAY[current_displayed_string] # Set text to the correct label
-	
-	# Button labels
-	# ALERT: Hardcoded strings!!!
-	match current_displayed_string:
-		1:
-			back_button.text = "Oh yeah."
-			next_button.text = "Huh???"
-		2:
-			back_button.text = "Back"
-			next_button.text = "Next"
-		4: # Yes, I know...
-			back_button.text = "Back"
-			next_button.text = "Next"
-		5:
-			next_button.text = "Okay."
-		6:
-			next_button.text = "OKAY!!!"
-	
-	if current_displayed_string == 0 or current_displayed_string == 7:
-		self.queue_free() # Delete the whole dang tutorial node
-
-# Button zone
 func _on_back_button_pressed() -> void:
-	current_displayed_string -= 1
+	if line_to_display != 23: # "Please repeat" should make the tutorial repeat itself from earlier
+		line_to_display -= 1
+	else:
+		line_to_display = 20 # "So, to recap..."
+	_refresh_text()
 
 func _on_next_button_pressed() -> void:
-	current_displayed_string += 1
+	line_to_display += 1
+	_refresh_text()
+
+func _should_be_shown(state) -> void:
+	if str(state) == "HIDE":
+		self.visible = false
+		mouse_input_blocker.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	else:
+		self.visible = true
+		mouse_input_blocker.mouse_filter = Control.MOUSE_FILTER_STOP
+
+func _refresh_text() -> void:
+	if line_to_display == -1 or line_to_display == 26: # Delete node if no more text
+		self.queue_free()
+	tutorial_text.text = tr("tutorial_str_%d" % line_to_display) # Set text
+	match line_to_display: # Change buttons (NOTE: Yes the duplicates are ugly)
+		0:
+			back_button.text = tr("tutorial_btn_0_intro")
+			next_button.text = tr("tutorial_btn_1_intro")
+		1:
+			back_button.text = tr("tutorial_btn_0_default")
+			next_button.text = tr("tutorial_btn_1_default")
+		20:
+			back_button.text = tr("tutorial_btn_0_default")
+			next_button.text = tr("tutorial_btn_1_default")
+		23:
+			back_button.text = tr("tutorial_btn_0_end")
+			next_button.text = tr("tutorial_btn_1_end")
+		24:
+			back_button.text = tr("tutorial_btn_0_default")
+			next_button.text = tr("tutorial_btn_1_default")
+	_move_to_new_pos()
+
+# ALERT: Ugly-yet-functional code zone.
+func _move_to_new_pos():
+	var tween = get_tree().create_tween()
+	tween.set_ease(Tween.EASE_OUT)
+	tween.set_trans(Tween.TRANS_SINE)
+	match line_to_display:
+		0:
+			tween.tween_property(tutorial_panel, "position", positions[0], 0.25)
+		1:
+			back_button.disabled = true # Disable the back button for shenanigan prevention
+			back_button.visible = false
+		5:
+			tween.tween_property(tutorial_panel, "position", positions[1], 0.25)
+		6:
+			_should_be_shown("HIDE") # Hide tutorial to allow the player to... play
+			tween.tween_property(tutorial_panel, "position", positions[0], 0.25)
+		7:
+			tween.tween_property(tutorial_panel, "position", positions[2], 0.25)
+		9:
+			tween.tween_property(tutorial_panel, "position", positions[0], 0.25)
+		13:
+			tween.tween_property(tutorial_panel, "position", positions[4], 0.25)
+		14:
+			_should_be_shown("HIDE")
+			tween.tween_property(tutorial_panel, "position", positions[0], 0.25)
+		15:
+			tween.tween_property(tutorial_panel, "position", positions[3], 0.25)
+		16:
+			tween.tween_property(tutorial_panel, "position", positions[1], 0.25)
+		19:
+			tween.tween_property(tutorial_panel, "position", positions[0], 0.25)
+		20:
+			back_button.disabled = true
+			back_button.visible = false
+		23:
+			back_button.disabled = false # Re-enable the back button
+			back_button.visible = true
+		24:
+			back_button.disabled = true
+			back_button.visible = false
+			tween.tween_property(tutorial_panel, "position", positions[4], 0.25)
